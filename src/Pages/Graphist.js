a0d79ee5e0a398e16a98bac8344181db62f91242
Fred011 <frederic.vannier01@gmail.com>
2020-08-21 01:02:57 +0200

    add animations on scroll for the work sections

--- src/Pages/Graphist.js
+++ src/Pages/Graphist.js
@@ -8,32 +8,41 @@ export class Graphist extends Component {
       <div className="work-wrapper">
         <div className="right-part">
           <div className="title-wrap-2">
-            <h1 className="big-title">
-              Gra
-              <br />
-              Phiste
-            </h1>
+            <Fade up delay={400}>
+              <h1 className="big-title">
+                Gra
+                <br />
+              </h1>
+            </Fade>
+            <Fade up delay={600}>
+              <h1 className="big-title-bottom">Phiste</h1>
+            </Fade>
             <Fade right>
               <div className="styled-forme-2"></div>
             </Fade>
           </div>
           <div className="previews-container-2">
-            <div className="previews-wrap-2">
-            <SlideshowBis />
-            </div>
+            <Fade left delay={600}>
+              <div className="previews-wrap-2">
+                <SlideshowBis />
+              </div>
+            </Fade>
           </div>
         </div>
         <div className="left-part">
-          <p className="job-description">
-            De formation bac +2 en Communication et Marketing et animé par la
-            passion du design, je me suis très vite spécialisé dans le
-            graphisme.
-            <br />
-            <br />
-            Fort de 7 ans d'expérience, je souhaite toujours m'améliorer et
-            apprendre de nouvelles tendances. Au cours de ces années, j'ai pu
-            réaliser des projets divers et variés que vous pouvez consulter ici.
-          </p>
+          <Fade up delay={700}>
+            <p className="job-description">
+              De formation bac +2 en Communication et Marketing et animé par la
+              passion du design, je me suis très vite spécialisé dans le
+              graphisme.
+              <br />
+              <br />
+              Fort de 7 ans d'expérience, je souhaite toujours m'améliorer et
+              apprendre de nouvelles tendances. Au cours de ces années, j'ai pu
+              réaliser des projets divers et variés que vous pouvez consulter
+              ici.
+            </p>
+          </Fade>
         </div>
       </div>
     );
--- src/Pages/Webdev.js
+++ src/Pages/Webdev.js
@@ -1,42 +1,42 @@
 import React from "react";
-import { Spring } from "react-spring/renderprops";
 import Fade from "react-reveal/Fade";
-import { Link } from "react-router-dom";
-import Slideshow from '../components/Slideshow';
+import Slideshow from "../components/Slideshow";
 
 export default function Webdev() {
   return (
     <div className="work-wrapper">
       <div className="left-part">
-        <p className="job-description">
-          Passinonné par le graphisme et les technologies web, c'est tout
-          naturellement que je me suis tournée vers le métier de développeur.
-          <br />
-          <br />
-          J'ai donc rejoins les rangs du bootcamp Ironhack, à Barcelone, pour
-          devenir Développeur Full Stack. Durant cette formation de 3 mois, j'ai
-          pu réaliser ces 3 projets ainsi que mon premier site personnel.
-        </p>
+        <Fade up delay={700}>
+          <p className="job-description">
+            Passinonné par le graphisme et les technologies web, c'est tout
+            naturellement que je me suis tournée vers le métier de développeur.
+            <br />
+            <br />
+            J'ai donc rejoins les rangs du bootcamp Ironhack, à Barcelone, pour
+            devenir Développeur Full Stack. Durant cette formation de 3 mois,
+            j'ai pu réaliser ces 3 projets ainsi que mon premier site personnel.
+          </p>
+        </Fade>
       </div>
       <div className="right-part">
         <div className="title-wrap">
-          <Spring from={{ opacity: 0, transitionDuration: 2000 }} to={{ opacity: 1 }}>
-            {(props) => (
-              <div style={props}>
-                <h1 className="big-title">
-                  Déve
-                  <br />
-                  loppeur
-                </h1>
-              </div>
-            )}
-          </Spring>
-          <Spring from={{ width: 0 }} to={{ width: 320 }}>
-            {(props) => <div style={props} className="styled-forme"></div>}
-          </Spring>
+          <Fade up delay={400}>
+            <h1 className="big-title">
+              Déve
+              <br />
+            </h1>
+          </Fade>
+          <Fade up delay={600}>
+            <h1 className="big-title-bottom">loppeur</h1>
+          </Fade>
+          <Fade left>
+            <div className="styled-forme"></div>
+          </Fade>
         </div>
         <div className="previews-container">
-          <Slideshow />
+          <Fade right delay={600}>
+            <Slideshow />
+          </Fade>
         </div>
       </div>
     </div>
