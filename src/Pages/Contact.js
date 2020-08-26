eadf9f6a6943ac7f7894aff39e33a17d66008aca
Fred011 <frederic.vannier01@gmail.com>
2020-08-26 11:36:05 +0200

    minor fix

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -21,7 +21,7 @@ export default function Contact() {
           </div>
           <div className="contact-text">
             <section className="first-section">
-              <p>Intéressé pour collaboration avec moi?</p>
+              <p>Intéressé pour une collaboration avec moi?</p>
             </section>
             <section className="second-section">
               <p>
